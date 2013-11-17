592e353202c56679a0bd6b5a2d14201b7b22e2a1
Felix Xiao <felix_xiao@yahoo.com>
2013-11-17 12:35:57 -0500

    only one pin

--- hackduke13/hackduke13/hackduke13/MapPostViewController.m
+++ hackduke13/hackduke13/hackduke13/MapPostViewController.m
@@ -62,7 +62,8 @@
     [self.jSON setObject:[NSString stringWithFormat:@"%f",touchMapCoordinate.latitude] forKey:@"latitude"];
     [self.jSON setObject:[NSString stringWithFormat:@"%f",touchMapCoordinate.longitude] forKey:@"longitude"];
     [annotation setTitle:@"New Post"]; //You can set the subtitle too
-    [self.mapView addAnnotation:annotation];
+    if (self.mapView.annotations.count < 2)
+        [self.mapView addAnnotation:annotation];
     
 }
 
@@ -89,82 +90,6 @@
 	[mv selectAnnotation:mp animated:YES];
 }
 
-
-
-- (void)loadMapView
-{
-    
-    NSMutableArray *venuesArray = [[NSMutableArray alloc] init];
-    
-    for (NSMutableDictionary *d in self.appDelegate.allContent) {
-        NSLog(@"POSITION 2");
-        NSMutableDictionary *venue = [[NSMutableDictionary alloc] init];
-        [venue setObject:[d objectForKey:@"latitude"] forKey:@"latitude"];
-        [venue setObject:[d objectForKey:@"longitude"] forKey:@"longitude"];
-        [venue setObject:[d objectForKey:@"username"] forKey:@"name"];
-        [venue setObject:[d objectForKey:@"timestamp"] forKey:@"address"];
-        [venue setObject:[d objectForKey:@"imgURL"] forKey:@"imgURL"];
-        [venue setObject:[d objectForKey:@"description"] forKey:@"description"];
-        NSLog(@"hi%@",venue);
-        [venuesArray addObject:venue];
-    }
-    
-    [self plotVenues:venuesArray];
-}
-
-- (void)plotVenues:(NSMutableArray*)searchResults {
-    //clear all the annotations
-    for (id<MKAnnotation> annotation in mapView.annotations) {
-        if (![annotation isKindOfClass:[MKUserLocation class]]) {
-            [mapView removeAnnotation:annotation];
-        }
-    }
-    
-    for (NSMutableDictionary *venueData in searchResults) {
-        CLLocationCoordinate2D coordinate;
-        coordinate.latitude = [[venueData objectForKey:@"latitude"] floatValue];
-        coordinate.longitude= [[venueData objectForKey:@"longitude"] floatValue];
-        //set up other parts of the annotation
-        MapViewAnnotation *venueAnnotation = [[MapViewAnnotation alloc] initWithName:[venueData objectForKey:@"name"] address:[venueData objectForKey:@"address"] coordinate:coordinate];
-        venueAnnotation.imgURL = [venueData objectForKey:@"imgURL"];
-        [mapView addAnnotation:venueAnnotation];
-	}
-}
-
-- (MKAnnotationView *)mapView:(MKMapView *)mapview viewForAnnotation:(id <MKAnnotation>)annotation
-{
-    if ([annotation isKindOfClass:[MKUserLocation class]])
-        return nil;
-    static NSString* AnnotationIdentifier = @"AnnotationIdentifier";
-    MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:AnnotationIdentifier];
-    if(annotationView)
-        return annotationView;
-    else
-    {
-        MKAnnotationView *annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation
-                                                                        reuseIdentifier:AnnotationIdentifier];
-        annotationView.canShowCallout = NO;
-        annotationView.image = [UIImage imageNamed:@"dot.png"];
-        UIButton* rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-        [rightButton addTarget:self action:@selector(showVenueDetails:) forControlEvents:UIControlEventTouchUpInside];
-        [rightButton setTitle:annotation.title forState:UIControlStateNormal];
-        
-        //UIView *leftButton = [[UIView alloc] initWithFrame:CGRectMake(0,0,80,80)];
-        //UIImageView *starsImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,80,80)];
-        //NSURL *myURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.felixxiao.com/uploads/%@",[((MapViewAnnotation*) annotation) imgURL]]];
-        //NSData *imageData = [NSData dataWithContentsOfURL:myURL];
-        //starsImageView.image = [UIImage imageWithData:imageData];
-        //[leftButton addSubview:starsImageView];
-        
-        //set tag equal to venueID
-        //annotationView.leftCalloutAccessoryView = leftButton;
-        //annotationView.rightCalloutAccessoryView = rightButton;
-        annotationView.canShowCallout = NO;
-        annotationView.draggable = YES;
-        return annotationView;
-    }
-    return nil;
-}
 
 
 
